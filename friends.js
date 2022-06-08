5ba90eded5612a528c22c2b175cbd9837e501897
Jenn Yacoubian <jyacoubian@gmail.com>
2022-06-08 17:54:02 -0600

    Add good friend if

--- friends.js
+++ friends.js
@@ -5,3 +5,13 @@ if (bestFriend === "Casey") {
 } else {
   console.log("Sorry, try again.");
 }
+
+var goodFriend = "Matt"
+
+if (goodFriend === "Casey") {
+  console.log("They are they best.");
+} else if (goodFriend === "Matt") {
+  console.log("He is so wise!");
+} else {
+  console.log("Sorry, try again.");
+}
